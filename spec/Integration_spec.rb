3506a6b45d7bca6c56010825b988c21891ddbdd5
cmtoups <cmtoups@me.com>
2012-12-16 14:13:06 -0500

    spec has been drastically we factored to include single assertions

--- spec/Integration_spec.rb
+++ spec/Integration_spec.rb
@@ -9,45 +9,104 @@ require_relative 'persistence/adapter/mock_helpers.rb'
 
 describe ::Persistence do
 
-  it 'can create some objects with sub-objects and get them back' do
-    
-    ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new )
+	before(:all)	{ ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new ) }
+	after(:all)		{ ::Persistence.disable_port( :mock ) }
+	
+	before :each  do 
+		@user = ::Persistence::Adapter::Abstract::Mock::User.new.persist!
+    @user.populate
+	end
+		
+	context "#top_object" do 
 
-    user = ::Persistence::Adapter::Abstract::Mock::User.new.persist!
-    user.populate
-    
-    user.persistence_bucket.should == ::Persistence::Adapter::Abstract::Mock::User.instance_persistence_bucket
-    user.persistence_bucket.name.should == ::Persistence::Adapter::Abstract::Mock::User.to_s.to_sym
-    
-    user.address.persistence_bucket.should == ::Persistence::Adapter::Abstract::Mock::User::Address.instance_persistence_bucket
-    user.address.persistence_bucket.name.should == ::Persistence::Adapter::Abstract::Mock::User::Address.to_s.to_sym
-    
-    user.alternate_address.persistence_bucket.should == ::Persistence::Adapter::Abstract::Mock::User::Address.instance_persistence_bucket
-    user.alternate_address.persistence_bucket.name.should == ::Persistence::Adapter::Abstract::Mock::User::Address.to_s.to_sym
-    
-    user.notes.persistence_bucket.should == ::Persistence::Adapter::Abstract::Mock::NotesArray.instance_persistence_bucket
-    user.notes.persistence_bucket.name.should == ::Persistence::Adapter::Abstract::Mock::NotesArray.to_s.to_sym
-    
-    user.dictionary.persistence_bucket.should == ::Persistence::Adapter::Abstract::Mock::DictionaryHash.instance_persistence_bucket
-    user.dictionary.persistence_bucket.name.should == ::Persistence::Adapter::Abstract::Mock::DictionaryHash.to_s.to_sym
-    
-    user.notes[0].persistence_bucket.should == ::Persistence::Adapter::Abstract::Mock::Note.instance_persistence_bucket
-    user.notes[0].persistence_bucket.name.should == ::Persistence::Adapter::Abstract::Mock::Note.to_s.to_sym
-    
-    notes_array = ::Persistence::Adapter::Abstract::Mock::NotesArray.persist( user.notes.persistence_id )
-    notes_array.should == user.notes
-    
-    dictionary_hash = ::Persistence::Adapter::Abstract::Mock::DictionaryHash.persist( user.dictionary.persistence_id )
-    dictionary_hash.should == user.dictionary
-    
-    persisted_user = ::Persistence::Adapter::Abstract::Mock::User.persist( user.persistence_id )
-    persisted_user.class.should == ::Persistence::Adapter::Abstract::Mock::User
-    
-    persisted_user.should == user
-    
-    ::Persistence.disable_port( :mock )
-    
-  end
+		it "should have a valid persistence_bucket" do
+		  @user.persistence_bucket.should == ::Persistence::Adapter::Abstract::Mock::User.instance_persistence_bucket
+		
+		end
+		
+		it "persistence_bucket name should be the full class name of its object" do
+		
+		  @user.persistence_bucket.name.should == ::Persistence::Adapter::Abstract::Mock::User.to_s.to_sym
+		
+		end
+		
+	end
+	
+
+	#To avoid redundant code the following integrations tests will loop over this object variable	
+	objects = [:sub_object, :multiple_sub_objects, :object_array, :object_hash]
+
+	object_method = {}
+	object_klass	= {}
+	
+	object_method[:sub_object]	= "address"
+	object_klass[:sub_object] 	= ::Persistence::Adapter::Abstract::Mock::User::Address
+	
+	object_method[:multiple_sub_objects]	= "alternate_address"
+	object_klass[:multiple_sub_objects] 	= ::Persistence::Adapter::Abstract::Mock::User::Address
+	
+	object_method[:object_array]	= "notes"
+	object_klass[:object_array] 	= ::Persistence::Adapter::Abstract::Mock::NotesArray
+	
+	object_method[:object_hash]	= "dictionary"
+	object_klass[:object_hash] 	= ::Persistence::Adapter::Abstract::Mock::DictionaryHash
+	
+	objects.each do |object|
+		context "##{object}" do 
+			it "should have a valid persistence_bucket"  do 
+			  
+			  @user.send(object_method[object]).persistence_bucket.should == object_klass[object].instance_persistence_bucket
+			
+			end
+			
+			it "persistence_bucket name should be the full class name of its object" do 
+			
+			  @user.send(object_method[object]).persistence_bucket.name.should == object_klass[object].to_s.to_sym
+			
+			end
+			context "#persisted" do 
+				let(:persisted_instance) { object_klass[object].persist( @user.send(object_method[object]).persistence_id )}
+				it "should be the original instance" do 
+						persisted_instance.should == @user.send(object_method[object])
+				end
+			end
+		end
+	end
+
+	context "#complex_object" do 
+	
+		it "should have a valid persistence_bucket" do 
+		  
+		  @user.notes[0].persistence_bucket.should == ::Persistence::Adapter::Abstract::Mock::Note.instance_persistence_bucket
+		
+		end
+		
+		it "persistence_bucket name should be the full class name of its object" do 
+		
+		  @user.notes[0].persistence_bucket.name.should == ::Persistence::Adapter::Abstract::Mock::Note.to_s.to_sym
+		
+		end
+		
+		context "#persisted" do 
+		
+			let(:persisted_user) { ::Persistence::Adapter::Abstract::Mock::User.persist( @user.persistence_id ) }
+		  
+		  it "should be the original instance" do 
+		    
+		    persisted_user.should == @user
+		    
+		  end
+		  
+			#redundant test given prior instance test
+		  it "should be the same class as the original object" do 
+		  
+		    persisted_user.class.should == ::Persistence::Adapter::Abstract::Mock::User
+		    
+		  end
+		  
+		end
+	
+	end
 
 end
 
