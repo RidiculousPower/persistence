e46b678d0b1be059d54651b948f54ded26349e88
cmtoups <cmtoups@me.com>
2012-12-16 16:37:09 -0500

    spec has been drastically we factored to include single assertions

--- spec/persistence/adapter/enable_disable_spec.rb
+++ spec/persistence/adapter/enable_disable_spec.rb
@@ -7,23 +7,50 @@ describe ::Persistence::Adapter::Abstract::EnableDisable do
   #  initialize  #
   ################
 
-  it 'can initialize with a home directory specified' do
+  home_directory_location = '/tmp/persistence_spec_home_directory'
+
+  before :all do 
+  	class ::Persistence::Adapter::Abstract::EnableDisable::Mock
+      include ::Persistence::Adapter::Abstract::EnableDisable
+    end
     class ::Persistence::Adapter::Abstract::EnableDisable::Mock
       include ::Persistence::Adapter::Abstract::EnableDisable
     end
-    home_directory_location = '/tmp/persistence_spec_home_directory'
-    ::Persistence::Adapter::Abstract::EnableDisable::Mock.new( home_directory_location ).instance_eval do
-      enabled?.should == false
-      enable
-      enabled?.should == true
-      disable
-      enabled?.should == false
-      enable
-      File.exists?( home_directory_location ).should == true
-      self.home_directory.should == home_directory_location
-      Dir.delete( home_directory_location )
-      File.exists?( home_directory_location ).should == false
-    end    
+    
   end
+  
+  after :each do 
+  	Dir.delete( home_directory_location )
+  end
+
+  let(:adapter) {::Persistence::Adapter::Abstract::EnableDisable::Mock.new( home_directory_location )}
+
+	#can initialize with a home directory specified
+  it "should not be enabled by default" do 
+  	adapter.enabled?.should == false
+  end
+
+  context "after enable" do 
+  	before :each do 
+  	  adapter.enable
+  	end
+  	
+	  it "should be enabled" do 
+      adapter.enabled?.should == true
+	  end
+	  it "should disable" do 
+      adapter.disable
+      adapter.enabled?.should == false
+	  end
+	  context "home_directory" do 
+		  it "should exist" do 
+	      File.exists?( adapter.home_directory ).should == true
+		  end
+		  it "should be the same directory as user defined" do       
+	      adapter.home_directory.should == home_directory_location
+		  end
+    end
+  end
+
 
 end
