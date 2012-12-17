78a3dcc12effeab7b51d9fbec5141d17480899e1
cmtoups <cmtoups@me.com>
2012-12-17 13:37:25 -0500

    spec has been refactored for better scalability

--- spec/persistence/adapter/primary_key/simple_spec.rb
+++ spec/persistence/adapter/primary_key/simple_spec.rb
@@ -6,11 +6,14 @@ describe ::Persistence::Adapter::Abstract::PrimaryKey::Simple do
   ####################################
   #  primary_key_for_attribute_name  #
   ####################################
-
-  it 'can be used for a simple primary key' do
-    class ::Persistence::Adapter::Abstract::PrimaryKey::Simple::Mock
+  
+  before :all do 
+  	class ::Persistence::Adapter::Abstract::PrimaryKey::Simple::Mock
       include ::Persistence::Adapter::Abstract::PrimaryKey::Simple
     end
+  end
+
+  it 'should persist with a simple primary key' do
     ::Persistence::Adapter::Abstract::PrimaryKey::Simple::Mock.new.instance_eval do
       primary_key_for_attribute_name( self, :attribute_name ).should == :attribute_name
     end
