f36ba8117654fca015378a484ba37c0b81b0559d
cmtoups <cmtoups@me.com>
2013-01-26 16:37:46 -0500

    Full spec rework, now using single assertions. Added 2 failing tests.

--- spec/persistence/cursor_spec.rb
+++ spec/persistence/cursor_spec.rb
@@ -13,8 +13,11 @@ describe ::Persistence::Cursor do
       explicit_index :key
     end
 
+    #None of these test do assignments, before :all will work just as well as before :each.
+
     @objects = [ ]
     @index = ::Persistence::Cursor::Mock.index( :key )
+
     5.times do |this_number|
       instance = ::Persistence::Cursor::Mock.new
       instance.name = 'Number ' << this_number.to_s
@@ -24,105 +27,170 @@ describe ::Persistence::Cursor do
       @objects.push( instance )
     end
 
-    @cursor = ::Persistence::Cursor::Mock.instance_persistence_bucket.cursor
+    @normal_cursor = ::Persistence::Cursor::Mock.instance_persistence_bucket.cursor
     @index_cursor = @index.cursor
 
   end
-  
+
   after :all do
     ::Persistence.disable_port( :mock )
   end
 
+  before :each do
+    @objects.first
+    @normal_cursor.first
+    @index_cursor.first
+  end
+
+
+  cursors = [:normal_cursor, :index_cursor]
+
   ################
   #  persisted?  #
   ################
+  context "#persisted?" do
+    it "with persistence_id should a return true if the object is in the normal cursor" do
+      @normal_cursor.persisted?( @objects[ 0 ].persistence_id ).should == true
+    end
 
-  it 'can report whether a key exists and set its position to the key if it does' do
-    @cursor.persisted?( @objects[ 0 ].persistence_id ).should == true
-    @index_cursor.persisted?( @objects[ 0 ].name ).should == true
-  end
-  
-  ###########
-  #  first  #
-  ###########
-  
-  it 'can set its position to the first key' do
-    @cursor.first.should == @objects.first
-    @cursor.first( 2 ).should == @objects.first( 2 )
-    @index_cursor.first.should == @objects.first
-    @index_cursor.first( 2 ).should == @objects.first( 2 )
-  end
-
-  ##########
-  #  last  #
-  ##########
-  
-  it 'can set its position to the last key' do
-    @cursor.last.should == @objects.last
-    @cursor.last( 2 ).should == @objects.last( 2 )
-    @index_cursor.first
-    @index_cursor.current.should == @objects.first
+    it "with object name should a return true if the object is in the index cursor" do
+      @index_cursor.persisted?( @objects[ 0 ].name ).should == true
+    end
   end
 
   #########
   #  any  #
   #########
-  
-  it 'can set its position to the last key' do
-    @objects.include?( @cursor.any ).should == true
-    @cursor.any( 2 ).each do |this_object|
-      @objects.include?( this_object ).should == true
+  context "#any" do
+    it "should return a object in the cursor" do
+      @objects.include?( @normal_cursor.any ).should == true
     end
-    @index_cursor.first.should == @objects[ 0 ]
-    @index_cursor.next.should == @objects[ 0 ]
-    @index_cursor.next.should == @objects[ 1 ]
-    @index_cursor.next( 2 ).should == @objects[ 2..3 ]
+
+    it "with 2 should return two objects in the cursor" do
+      @normal_cursor.any( 2 ).each do |this_object|
+        @objects.include?( this_object ).should == true
+      end
+    end
+
+    it "should return a random value"
+
   end
 
-  #############
-  #  current  #
-  #############
-  
-  it 'can return the current key' do
-    @cursor.first
-    @cursor.current.should == @objects.first
+  #################
+  #  first, last  #
+  #################
+  [:first, :last].each do |method|
+    context "##{method}" do
+      cursors.each do |object|
+        let(:var) { instance_variable_get("@#{object}")}
+        context "with #{ object }" do
+          it "should return the #{method} object" do
+            var.send(method).should == @objects.send(method)
+          end
+
+          it "should return the #{method} 2 object" do
+            var.send(method, 2 ).should == @objects.send(method, 2 )
+          end
+        end
+      end
+    end
   end
 
   ##########
   #  next  #
   ##########
-  
-  it 'can set its position to the next key' do
-    @cursor.first.should == @objects[ 0 ]
-    @cursor.next.should == @objects[ 0 ]
-    @cursor.next.should == @objects[ 1 ]
-    @cursor.next( 2 ).should == @objects[ 2..3 ]
+  context "#next" do
+    #can set its position to the next key
+    cursors.each do |object|
+      context "- #{ object } -" do
+        let(:var) { instance_variable_get("@#{object}") }
+
+        it "first call should return the 0th object" do
+          var.next.should == @objects.first
+        end
+
+        it "should return the next object in the sequence" do
+          5.times do |index|
+            var.next.should == @objects[index]
+          end
+        end
+
+        it "first call with numeric argument should return the next number of objects in the sequence equal to the numeric argument" do
+          5.times do |index|
+            var.first
+            var.next( index ).should == @objects[ 0..index-1 ] if index > 1 # next of 0 or 1 is not an array
+          end
+        end
+
+        it "with the argument of 1 should return the next object in the sequence" do
+          var.next( 1 ).should == @objects.first
+        end
+
+        it "with the argument of 0 should return nil" do
+          var.next( 0 ).should == nil
+        end
+
+        it "with the argument of -1 should be treated as an absolute value and return the next object in the sequence" do
+          var.next( -1 ).should == @objects.first
+        end
+
+      end
+    end
+  end
+
+  #############
+  #  current  #
+  #############
+  context "#current" do
+    it 'should return the current key' do
+      @normal_cursor.current.should == @objects.first
+    end
+
+    it 'should return the current key' do
+      @normal_cursor.next(2)
+      @normal_cursor.current.should == @objects[1]
+    end
   end
 
   ##########
   #  each  #
   ##########
+  context "#each" do
+    #can iterate each record
+    cursors.each do |object|
+      context "- #{ object } -" do
 
-  it 'can iterate each record' do
-    @cursor.first
-    @cursor.each_with_index do |this_object, this_index|
-      this_object.should == @objects[ this_index ]
+        let(:enum) {instance_variable_get("@#{object}").each}
+
+        it "should return an enumerator" do
+          enum.is_a?( Enumerator ).should == true
+        end
+
+        context "returned enumerator" do
+          it "with next should return the first object" do
+            enum.next.should == @objects[ 0 ]
+          end
+
+          it "with next twice should return the second object" do
+            enum.next
+            enum.next.should == @objects[ 1 ]
+          end
+        end
+      end
     end
-    @cursor.first
-    enum = @cursor.each
-    enum.is_a?( Enumerator ).should == true
-    enum.next.should == @objects[ 0 ]
-    enum.next.should == @objects[ 1 ]
-    @index_cursor.first
-    @index_cursor.each_with_index do |this_object, this_index|
-      this_object.should == @objects[ this_index ]
+  end
+
+  context "#each_with_index" do
+    #can iterate each record
+    cursors.each do |object|
+      context "- #{ object } -" do
+        it "should return each object and index key pair" do
+          instance_variable_get("@#{object}").each_with_index do |this_object, this_index|
+            this_object.should == @objects[ this_index ]
+          end
+        end
+      end
     end
-    @index_cursor.first
-    enum = @index_cursor.each
-    enum.is_a?( Enumerator ).should == true
-    enum.next.should == @objects[ 0 ]
-    enum.next.should == @objects[ 1 ]
   end
 
 end
-
