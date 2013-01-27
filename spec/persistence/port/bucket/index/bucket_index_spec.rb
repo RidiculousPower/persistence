44486a271c831d6ce6fea77e01bacb1ae5fc7d46
cmtoups <cmtoups@me.com>
2013-01-26 20:34:12 -0500

    Full spec rework, now using single assertions. Added 1 pending test.

--- spec/persistence/port/bucket/index/bucket_index_spec.rb
+++ spec/persistence/port/bucket/index/bucket_index_spec.rb
@@ -3,12 +3,16 @@ require_relative '../../../../../lib/persistence.rb'
 
 describe ::Persistence::Port::Bucket::BucketIndex do
 
+  after(:each) do
+    ::Persistence.disable_port( :mock )
+  end
+
   it 'is a block index that runs on all inserts into a bucket' do
     class ::Persistence::Port::Bucket::BucketIndex::Mock
       include ::Persistence::Object
       attr_accessor :some_value
     end
-    
+
     object = ::Persistence::Port::Bucket::BucketIndex::Mock.new
 
     ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new )
@@ -16,10 +20,8 @@ describe ::Persistence::Port::Bucket::BucketIndex do
     index_instance = object.persistence_bucket.create_index( :bucket_index ) do |object|
       object.some_value
     end
-    
-    index_instance.is_a?( ::Persistence::Port::Bucket::BucketIndex ).should == true
 
-    ::Persistence.disable_port( :mock )
+    index_instance.is_a?( ::Persistence::Port::Bucket::BucketIndex ).should == true
 
   end
 
@@ -30,38 +32,85 @@ describe ::Persistence::Port::Bucket::BucketIndex do
   #  index         #
   ##################
 
-  it 'can create an index and then separately retrieve that index' do
-    # before port is open    
-    class ::Persistence::Port::Bucket::BucketIndex::Mock
-      include ::Persistence::Object
-      attr_atomic_accessor :some_value, :some_other_value
-    end
-    
-    bucket = ::Persistence::Port::Bucket::BucketIndex::Mock.instance_persistence_bucket
-    index_instance = bucket.create_index( :index ) do |object|
-      object.some_value
-    end
-    Proc.new { index_instance.adapter_index }.should raise_error
-    bucket.index( :index ).should == index_instance
+  context "#has_index?" do
+    it "should have a spec. If this is still relevant..."
+  end
 
-    # after port is open
-    ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new )
-    index_instance_two = bucket.create_index_with_duplicates( :other_index ) do |object|
-      object.some_other_value
+  context '#create_index' do
+    #can create an index and then separately retrieve that index
+    before :each do
+      class ::Persistence::Port::Bucket::BucketIndex::Mock
+        include ::Persistence::Object
+        attr_atomic_accessor :some_value, :some_other_value
+      end
+      @bucket = ::Persistence::Port::Bucket::BucketIndex::Mock.instance_persistence_bucket
+      @index_instance = @bucket.create_index( :index ) do |object|
+        object.some_value
+      end
     end
-    index_instance_two.adapter_index.should_not == nil
-    bucket.index( :other_index ).should == index_instance_two
-    
-    instance = ::Persistence::Port::Bucket::BucketIndex::Mock.new
-    instance.some_value = :a_value
 
-    bucket.put_object!( instance )
-    bucket.index( :index ).index_object( instance )
-    bucket.count.should == 1
-    bucket.index( :index ).count.should == 1
-    bucket.index( :other_index ).count.should == 0
-    
-    ::Persistence.disable_port( :mock )
+    it "should return a copy of the new index" do
+      @bucket.index( :index ).should == @index_instance
+    end
+
+    context "- before port is open -" do
+      it "should raise error if index is referenced" do
+        Proc.new { @index_instance.adapter_index }.should raise_error
+      end
+    end
+
+    context "- after port is open -" do
+      before :each do
+        ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new )
+        @instance = ::Persistence::Port::Bucket::BucketIndex::Mock.new
+        @instance.some_value = :a_value
+        @bucket.put_object!( @instance )
+      end
+
+      context "#index_object" do
+        it "should index the object, #count should increment by 1" do
+          @bucket.index( :index ).index_object( @instance )
+          @bucket.count.should == 1
+        end
+
+        it "should index the object, #count(:index) should increment by 1" do
+          @bucket.index( :index ).index_object( @instance )
+          @bucket.index( :index ).count.should == 1
+        end
+      end
+      context "#count" do
+        it "should be 0 when no object have been added to the index" do
+          @bucket.create_index( :other_index ) do |object|
+            object.some_value
+          end
+          @bucket.index( :other_index ).count.should == 0
+        end
+      end
+    end
+  end
+
+  context "#create_index_with_duplicates" do
+    before :each do
+      class ::Persistence::Port::Bucket::BucketIndex::Mock
+        include ::Persistence::Object
+        attr_atomic_accessor :some_value, :some_other_value
+      end
+      @bucket = ::Persistence::Port::Bucket::BucketIndex::Mock.instance_persistence_bucket
+      @index_with_duplicates_instance = @bucket.create_index_with_duplicates( :index_with_duplicate ) do |object|
+        object.some_other_value
+      end
+    end
+
+    it "should return a copy of the new index" do
+      @bucket.index( :index_with_duplicate ).should == @index_with_duplicates_instance
+    end
+
+    context "- after port is open -" do
+      it "should create an index" do
+        ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new )
+        @index_with_duplicates_instance.adapter_index.should_not == nil
+      end
+    end
 
   end
 
