01b6b13ebd4840f5c942ccc8b450bb82fca73f30
cmtoups <cmtoups@me.com>
2013-01-26 23:19:13 -0500

    Merged logic from indexing_spec.rb and class_instance_spec.rb into flat_rspec_helpers.rb. Now their logic is tested on all flat files. indexing_spec.rb now contains a single block_index test and class_instance_spec.rb has been removed.

--- spec/persistence/object/flat/flat_rspec_helpers.rb
+++ spec/persistence/object/flat/flat_rspec_helpers.rb
@@ -5,6 +5,7 @@ shared_examples "a flat object with Persistence" do
   end
 
   after :each do
+    object.cease! #would be better if mock memory was dumped
     ::Persistence.disable_port( :mock )
   end
 
@@ -15,8 +16,9 @@ shared_examples "a flat object with Persistence" do
   end
 
   context "#persist!" do
-    it "should persist without error" do
+    it "should persist with persistence_id" do
       object.persist!
+      object.persistence_id.should_not == nil
     end
   end
 
@@ -24,6 +26,11 @@ shared_examples "a flat object with Persistence" do
     it "should return the object" do
       object.persist!
       object.class.persist( object.persistence_id ).should == object
+    end
+
+    it "should return the first object given argument 0" do
+      object.persist!
+      object.class.persist( 0 ).should == object
     end
   end
 
@@ -35,8 +42,40 @@ shared_examples "a flat object with Persistence" do
     end
   end
 
+  context "#persist?" do
+    it "should not return true if there is no persisted object" do
+      begin
+        object.class.persisted?( object ).should == false  
+      rescue ArgumentError => e
+        p "'should not return true if there is no persisted object.' Passed by ArgumentError, did not return true: #{e}"
+      end
+    end
+
+    it "should return the true given at least one persisted object and argument 0" do
+      object.persist!
+      object.class.persisted?( 0 ).should == true
+    end
+
+    it "should return true if the object has been persisted" do
+      object.persist!
+      object.class.persisted?( object.persistence_id ).should == true    
+    end
+
+    it "should not return true if the object has been ceased" do
+      object.persist!
+      persistence_id = object.persistence_id
+      object.cease!
+      object.class.persist( persistence_id ).should == nil
+    end
+  end
+
   context "with index key" do
-    it "should persist! and persist" do
+    it "should persist!" do
+      object.persist!( :explicit_index => storage_key )
+      object.persistence_id.should_not == nil
+    end
+
+    it "should persist" do
       object.persist!( :explicit_index => storage_key )
       object.class.persist( :explicit_index => storage_key ).should == object
     end
--- spec/persistence/object/flat/indexing_spec.rb
+++ spec/persistence/object/flat/indexing_spec.rb
@@ -1,21 +1,10 @@
 
 require_relative '../../../../lib/persistence.rb'
+require_relative './flat_rspec_helpers.rb'
 
 describe ::Persistence::Object::Flat do
   
   before :all do
-    ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new )  
-  end
-  
-  after :all do
-    ::Persistence.disable_port( :mock )  
-  end
-  
-  #############
-  #  persist  #
-  #############
-  
-  it 'can persist a flat object and test whether it has been persisted' do
     class ::Persistence::Object::Flat::Mock < ::String
       include ::Persistence::Object::Flat
       explicit_index :explicit_index
@@ -23,16 +12,26 @@ describe ::Persistence::Object::Flat do
         chars.first
       end
     end
-    instance = ::Persistence::Object::Flat::Mock.new
-    instance.persist!
-    instance.persistence_id.should_not == nil
-    ::Persistence::Object::Flat::Mock.persisted?( instance.persistence_id ).should == true
-    ::Persistence::Object::Flat::Mock.persist( instance.persistence_id ).should == instance
-    instance_two = ::Persistence::Object::Flat::Mock.new( 'test' )
-    instance_two.persist!( :explicit_index => :some_value )
-    instance_two.persistence_id.should_not == nil
-    ::Persistence::Object::Flat::Mock.persist( :explicit_index, :some_value ).should == instance_two
-    ::Persistence::Object::Flat::Mock.persist( :first_letter => 't' ).should == instance_two
+  end
+  
+  it_behaves_like "a flat object with Persistence" do
+    let(:object) { ::Persistence::Object::Flat::Mock.new }
+    let(:storage_key) { :some_value }
+  end
+
+  #############
+  #  block_index  #
+  #############
+  context "block_index" do
+    it 'should return instance given expected logic' do
+      ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new )  
+
+      instance = ::Persistence::Object::Flat::Mock.new( 'test' )
+      instance.persist!( :explicit_index => :some_value )
+      ::Persistence::Object::Flat::Mock.persist( :first_letter => 't' ).should == instance
+
+      ::Persistence.disable_port( :mock )  
+    end
   end
     
 end
