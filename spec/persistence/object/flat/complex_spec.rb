cbf9cf0e4a4981e2ba727594d0abeaa14599a454
cmtoups <cmtoups@me.com>
2013-01-26 21:56:27 -0500

    Flat Complex now use: it_behaves_like 'a flat object with Persistence'

--- spec/persistence/object/flat/complex_spec.rb
+++ spec/persistence/object/flat/complex_spec.rb
@@ -1,36 +1,19 @@
 
 require_relative '../../../../lib/persistence.rb'
+require_relative './flat_rspec_helpers.rb'
 
 describe Complex do
   
   before :all do
-    ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new )
-  end
-  
-  after :all do
-    ::Persistence.disable_port( :mock )
-  end
-
-  it "can put a complex number object to a persistence port and get it back" do
     class Complex
       include ::Persistence::Object::Flat
       explicit_index :explicit_index
     end
+  end
 
-    complex_object  = Complex( 42, 1 )
-    storage_key     = Complex( 37, 12 )
-    complex_object.persist!
-    Complex.persist( complex_object.persistence_id ).should == complex_object
-    complex_object.cease!
-    Complex.persist( complex_object.persistence_id ).should == nil
-
-    complex_object  = Complex( 42, 1 )
-    storage_key     = Complex( 37, 12 )
-    complex_object.persist!( :explicit_index, storage_key )
-    Complex.persist( :explicit_index, storage_key ).should == complex_object
-    Complex.cease!( :explicit_index, storage_key )
-    Complex.persist( :explicit_index, storage_key ).should == nil
-
+  it_behaves_like "a flat object with Persistence" do
+    let(:object) { Complex( 42, 1 ) }
+    let(:storage_key) { Complex( 37, 12 ) }
   end
   
 end
