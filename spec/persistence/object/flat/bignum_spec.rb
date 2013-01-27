14d1fdc80d626ae93266515a154f553c37619bdd
cmtoups <cmtoups@me.com>
2013-01-26 20:57:56 -0500

    Full spec rework, now using single assertions.

--- spec/persistence/object/flat/bignum_spec.rb
+++ spec/persistence/object/flat/bignum_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../../../lib/persistence.rb'
 
 describe Bignum do
   
-  before :all do
+  before :each do
 
     ::Persistence.enable_port( :mock, ::Persistence::Adapter::Mock.new )
 
@@ -11,31 +11,47 @@ describe Bignum do
       include ::Persistence::Object::Flat
       explicit_index :explicit_index
     end
-
+    @bignum_object = 10**20
   end
   
-  after :all do
+  after :each do
     ::Persistence.disable_port( :mock )
   end
 
-  it "can put a bignum number object to a persistence port and get it back" do
-    
-    bignum_object = 10**20
-    bignum_object.persist!
-    Bignum.persist( bignum_object.persistence_id ).should == bignum_object
-    bignum_object.cease!
-    Bignum.persist( bignum_object.persistence_id ).should == nil
-
+  context "#persist!" do
+    it "should persist without error" do
+      @bignum_object.persist!
+    end    
   end
 
-  it "can put a bignum number object to a persistence port and get it back via an indexed value" do
-    bignum_object = 10**20
-    storage_key   = 10**40
-    bignum_object.persist!( :explicit_index => storage_key )
-    Bignum.persist( :explicit_index => storage_key ).should == bignum_object
-    Bignum.cease!( :explicit_index => storage_key )
-    Bignum.persist( :explicit_index => storage_key ).should == nil
+  context "#persist" do
+    it "should return the bignum" do
+      @bignum_object.persist!
+      Bignum.persist( @bignum_object.persistence_id ).should == @bignum_object
+    end
+  end
+  
+  context "#cease!" do
+    it "should delete the bignum" do
+      @bignum_object.persist!
+      @bignum_object.cease!
+      Bignum.persist( @bignum_object.persistence_id ).should == nil
+    end
+  end
 
+  context "with index" do
+    it "should persist" do
+      #can put a bignum number object to a persistence port and get it back via an indexed value
+      storage_key   = 10**40
+      @bignum_object.persist!( :explicit_index => storage_key )
+      Bignum.persist( :explicit_index => storage_key ).should == @bignum_object
+    end
+    it "should cease!" do
+      storage_key   = 10**40
+      @bignum_object.persist!( :explicit_index => storage_key )
+      Bignum.cease!( :explicit_index => storage_key )
+      Bignum.persist( :explicit_index => storage_key ).should == nil
+    end
   end
   
 end
